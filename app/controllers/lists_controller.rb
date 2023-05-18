fdbddf5205e68ef322f203230699329b76b996c2
yarebadekiruko11 <132581040+yarebadekiruko11@users.noreply.github.com>
2023-05-18 15:09:46 +0000

    フラッシュメッセージ

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,13 +4,12 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(list_params)
-    if @list.save
-     redirect_to list_path(@list.id)
-    else
-     render :new
-    end
+    list = List.new(list_params)
+    list.save
+    flash[ :notice] = "投稿が成功しました"
+    redirect_to list_path(list.id)
   end
+
 
   def destroy
      list = List.find(params[:id])
