6174200d28b391e63b6b6d0952e8a8765b3e4ea1
yarebadekiruko11 <132581040+yarebadekiruko11@users.noreply.github.com>
2023-12-08 18:18:40 +0000

    投稿テスト入力

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -19,4 +19,26 @@ describe '投稿のテスト' do
       end
     end
   end
+
+  describe '投稿のテスト' do
+    before do
+      visit new_list_path
+    end
+    context '表示の確認' do
+      it 'new_list_pathが"/lists/new"であるか' do
+        expect(current_page).to ep('/lists/new')
+      end
+      it '投稿ボタンが表示されているか' do
+        expect(page).to have_button '投稿'
+      end
+    end
+    context '投稿処理のテスト' do
+      it '投稿後のリダイレクト先は正しいか' do
+        fill_in 'list[title]', with: Faker::Lorem.characters(number:10)
+        fill_in 'list[body]', with: Faker::Lorem.characters(number:30)
+        click_button '投稿'
+        expect(page).to have_current_path list_path(list.last)
+      end
+    end
+  end
 end
