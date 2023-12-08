ac29a6209f4829ae2d376b7c9ddc7054f31f0e4a
yarebadekiruko11 <132581040+yarebadekiruko11@users.noreply.github.com>
2023-12-08 17:57:21 +0000

    Topページテストコード入力

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -2,3 +2,21 @@
 
 require 'rails_helper'
 
+describe '投稿のテスト' do
+  let!(:list) { create(:list, titel:'hoge',body:'body') }
+
+
+  describe 'トップ画面のテスト' do
+    before do
+      visit top_path
+    end
+    context '表示の確認' do
+      it 'トップ画面(top_path)に｢ここはTopページです｣が表示されているか' do
+        expect(page).to have_content 'ここはTopページです'
+      end
+      it 'top_pathが"/top"であるか' do
+        expect(current_page).to eq('/top')
+      end
+    end
+  end
+end
