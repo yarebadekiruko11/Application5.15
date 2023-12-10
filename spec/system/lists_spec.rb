cfa84e1ffbdf34e92bb8e7bfe2cbffc2b23b64c5
yarebadekiruko11 <132581040+yarebadekiruko11@users.noreply.github.com>
2023-12-10 12:46:04 +0000

    詳細画面テストコード記入

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -4,8 +4,6 @@ require 'rails_helper'
 
 describe '投稿のテスト' do
   let!(:list) { create(:list, title:'hoge',body:'body') }
-
-
   describe 'トップ画面のテスト' do
     before do
       visit top_path
@@ -20,7 +18,7 @@ describe '投稿のテスト' do
     end
   end
 
-  describe '投稿のテスト' do
+  describe '投稿画面のテスト' do
     before do
       visit new_list_path
     end
@@ -38,6 +36,39 @@ describe '投稿のテスト' do
         fill_in 'list[body]', with: Faker::Lorem.characters(number:30)
         click_button '投稿'
         expect(page).to have_current_path list_path(list.last)
+      end
+    end
+  end
+
+  describe '一覧画面のテスト' do
+    before do
+      visit lists_path
+    end
+    context '一覧表示とリンクの確認' do
+      it '一覧表示画面に投稿されたものが表示されているか' do
+        except(page).to have_content list.title
+        except(page).to have_link list.title
+      end
+    end
+  end
+
+  describe '詳細画面のテスト' do
+    before do
+      visit list_path(list)
+    end
+    context '表示の確認' do
+      it '削除リンクが存在しているか' do
+        except(page).to have_link '削除'
+      end
+      it '編集リンクが存在しているか' do
+        except(page).to have_link '編集'
+      end
+    end
+    context 'リンクの遷移先の確認' do
+      it 'リンクの遷移先は編集画面か' do
+        edit_link = find_all('a')[3]
+        edit_link.click
+        except(current_path).to eq('/lists/' + list.id.to_s + '/edit')
       end
     end
   end
